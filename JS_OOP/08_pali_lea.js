bdf1870e1c56d485f7ddfc671629606fa1dd80ed
lea27 <lea27@tajamar365.com>
2019-07-02 21:28:57 +0200

    palindromo

---
+++ JS_OOP/08_pali_lea.js
@@ -0,0 +1,22 @@
+/**
+ * @description: funcion que dertermine si una cadena de texto es un palindromo
+ * @param {string} frase
+ * @return  true 
+ */
+
+ let frase
+ function ispalindromo(frase) {
+     let frase = []
+     for (let i = 0; i < frase.length; i++) {
+         const item = frase[i];
+         if ( nombre.toUpperCase() == item.toUpperCase())} else {
+
+         if (frase == frase.reverse()) {
+             return true
+             
+         }
+         
+     }
+     return false
+ }
+
