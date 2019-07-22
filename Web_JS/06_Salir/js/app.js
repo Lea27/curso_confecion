527a5d1d3a486cbbc92e56e0aec33a7cae1c9a9e
lea27 <lea27@tajamar365.com>
2019-07-22 20:56:23 +0200

    crono

--- Web_JS/06_Salir/js/app.js
+++ Web_JS/06_Salir/js/app.js
@@ -1,12 +1,34 @@
-export function app() {
-    console.log('Cargada app')
+import { Crono } from "./crono.js";
 
+export function app() {
+    console.log('Cargada app') 
+    
     let btnSalir = document.querySelector('#btn-salir')
+    let dlgCancelar = document.querySelector('#dlg-cancelar')
+    let btnCancelar = document.querySelector('#btn-cancelar')
+    let dlgTime = document.querySelector("#dlg-time")
+    let handlerSalir
+    let handlerInterval
 
     btnSalir.addEventListener('click', onClick)
+    btnCancelar.addEventListener('click', onCancel)
 
-    function onClick(event) {
+    new Crono()
+
+    function onClick() {
         let url = 'https://www.google.com/'
-        location.assign(url)
+        handlerSalir = setTimeout(() => location.assign(url),
+                    4800)
+        dlgCancelar.showModal()
+        dlgTime.value = 5
+        handlerInterval = setInterval( () => {
+            dlgTime.value -= 1
+        }, 1000)
     }
-}
+
+    function onCancel() {
+        clearTimeout(handlerSalir)
+        clearInterval(handlerInterval)
+        dlgCancelar.close()
+    }
+ }
--- Web_JS/06_Salir/js/bootstrap.js
+++ Web_JS/06_Salir/js/bootstrap.js
@@ -1,2 +1,7 @@
+
+
+
+
+
 import { app } from './app.js'
 document.addEventListener('DOMContentLoaded', app)
